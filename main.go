d7c2d96f8239dd22c88bf558e034d11a517b87a1
Paul-wg <42257051+Paul-wg@users.noreply.github.com>
2022-10-13 11:09:44 +1100

    test change main.go by WG

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello WG!!!"}
+		res := &response{Message: "Hello WG, have a good test!!!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
